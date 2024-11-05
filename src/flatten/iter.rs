a989fdccfc095c882171fa6114f4c10186d62200
al8n <scygliu1@gmail.com>
2024-11-05 20:52:22 +0800

    Re-exports `Comparable` and `Equivalent`

--- src/flatten/iter.rs
+++ src/flatten/iter.rs
@@ -1,39 +1,9 @@
-use core::{
-  cmp::Ordering,
-  ops::{Bound, RangeBounds},
+use {
+  super::{Entry, Query, SkipMap, VersionedEntry},
+  crate::ComparableRangeBounds,
+  core::ops::{Bound, RangeBounds},
+  crossbeam_skiplist::Comparable,
 };
-
-use crossbeam_skiplist::Comparable;
-
-use super::{Entry, Query, SkipMap, VersionedEntry};
-
-/// `ComparableRangeBounds` is implemented as an extention to `RangeBounds` to
-/// allow for comparison of items with range bounds.
-trait ComparableRangeBounds<Q: ?Sized>: RangeBounds<Q> {
-  /// Returns `true` if `item` is contained in the range.
-  fn compare_contains<K>(&self, item: &K) -> bool
-  where
-    Q: Comparable<K>,
-    K: ?Sized,
-  {
-    (match self.start_bound() {
-      Bound::Included(start) => start.compare(item) != Ordering::Greater,
-      Bound::Excluded(start) => start.compare(item) == Ordering::Less,
-      Bound::Unbounded => true,
-    }) && (match self.end_bound() {
-      Bound::Included(end) => end.compare(item) != Ordering::Less,
-      Bound::Excluded(end) => end.compare(item) == Ordering::Greater,
-      Bound::Unbounded => true,
-    })
-  }
-}
-
-impl<R, T> ComparableRangeBounds<T> for R
-where
-  R: ?Sized + RangeBounds<T>,
-  T: ?Sized,
-{
-}
 
 pub(super) struct BaseIter<'a, K, V, Q: ?Sized = K, R = core::ops::RangeFull> {
   pub(super) map: &'a SkipMap<K, V>,
